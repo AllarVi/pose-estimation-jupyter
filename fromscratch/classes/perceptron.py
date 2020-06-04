5f8a000f813762b47c8d66b5b8d7650a2e42ec2a
Allar <allar.viinamae@hotmail.com>
2020-06-04 17:29:15 +0300

    Add perceptron.py

---
+++ fromscratch/classes/perceptron.py
@@ -0,0 +1,23 @@
+class Perceptron:
+
+    def predict(self, row, weights):
+        bias = weights[0]
+        weights = weights[1:]
+        x = row[:-1]
+
+        state = self.get_state(bias, x, weights)
+
+        return self.get_activation(state)
+
+    def get_activation(self, state):
+        return 1.0 if state >= 0.0 else 0.0
+
+    def get_state(self, bias, x, weights):
+        weighted_input = self.get_weighted_input(x, weights)
+
+        state = weighted_input + bias
+
+        return state
+
+    def get_weighted_input(self, x, weights):
+        return sum([weights[i] * x[i] for i in range(len(x))])
