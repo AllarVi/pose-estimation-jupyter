c4bf52a481f200cda8285c10dfaaf441a32d0299
Allar <allar.viinamae@hotmail.com>
2020-06-06 17:35:00 +0300

    refactor back propagation

--- fromscratch/classes/feed_forward_network.py
+++ fromscratch/classes/feed_forward_network.py
@@ -44,3 +44,42 @@ class FeedForwardNetwork():
             inputs = new_inputs
 
         return inputs
+
+    # Calculate the derivative of an neuron output
+    @staticmethod
+    def transfer_derivative(output):
+        return output * (1.0 - output)
+
+    # Backpropagate error and store in neurons
+    def backward_propagate_error(self, expected):
+        layers = self.layers
+
+        for i in reversed(range(len(layers))):
+            layer = layers[i]
+            errors = list()
+
+            last_layer_idx = len(layers) - 1
+            if FeedForwardNetwork.is_last_layer(i, last_layer_idx):
+                errors = FeedForwardNetwork.get_last_layer_errors(expected, layer)
+            else:
+                for current_layer_neuron_idx in range(len(layer)):
+                    prev_layer = layers[i + 1]
+                    error = FeedForwardNetwork.get_error_for_current_layer_neuron(current_layer_neuron_idx,
+                                                                                  prev_layer)
+                    errors.append(error)
+
+            for j, neuron in enumerate(layer):
+                neuron['delta'] = errors[j] * FeedForwardNetwork.transfer_derivative(neuron['output'])
+
+    @staticmethod
+    def get_error_for_current_layer_neuron(current_layer_neuron_idx, prev_layer):
+        return sum([prev_layer_neuron['weights'][current_layer_neuron_idx] * prev_layer_neuron['delta']
+                    for prev_layer_neuron in prev_layer])
+
+    @staticmethod
+    def get_last_layer_errors(expected, layer):
+        return [(expected[i] - neuron['output']) for i, neuron in enumerate(layer)]
+
+    @staticmethod
+    def is_last_layer(current_layer_idx, last_layer_idx):
+        return current_layer_idx == last_layer_idx
